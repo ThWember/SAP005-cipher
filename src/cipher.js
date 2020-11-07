a0b3c525a8b486d078dddd17ce2b0ca528ef732c
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-11-07 15:06:04 -0300

    Updated cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,39 @@
-//fazer tudo de js no mesmo arquivo de pois pensar em separar
+let insertedTextEncode;
+let numberOffsetEncode;
+let cipherText = " ";
+let upperFirstASC = 65;
+
+let insertedTextDecode;
+let numberOffsetDecode;
+let decipherText = " ";
+let upperLastASC = 90;
+
+let alphabetLength = 26;
+
 
 const cipher = {
-  //...
+  encode: function () {
+    insertedTextEncode = document.getElementById("inserted-text-encode").value.toUpperCase();
+    numberOffsetEncode = Number(document.getElementById("number-offset-encode").value);
+    for (let i = 0; i <insertedTextEncode.length; i++){
+      let letterCodeChipher = (((insertedTextEncode[i].charCodeAt(0) - upperFirstASC+ Number(numberOffsetEncode)) % alphabetLength) + upperFirstASC);
+      let resultCipher = String.fromCharCode(letterCodeChipher);
+      cipherText += resultCipher;
+    }
+    document.getElementById("final-text-encode").innerHTML = cipherText;
+  },
+  decode: function () {
+      insertedTextDecode = document.getElementById("inserted-text-decode").value.toUpperCase();
+      numberOffsetDecode = Number(document.getElementById("number-offset-decode").value);
+      decipherText = " ";
+    
+      for (let i = 0; i <insertedTextDecode.length; i++){
+        const letterCodeDechipher = (((insertedTextDecode[i].charCodeAt(0) - upperLastASC - Number(numberOffsetDecode)) % alphabetLength) + upperLastASC);
+        const resultDecipher = String.fromCharCode(letterCodeDechipher);
+        decipherText += resultDecipher;
+      }
+      document.getElementById("final-text-decode").textContent = decipherText;
+    }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
-let insertedTextEncode;
-let numberOffsetEncode;
-let cipherText = " ";
-let upperFirstASC = 65;
-//let lowerFirstASC = 97;
+// let insertedTextEncode;
+// let numberOffsetEncode;
+// let cipherText = " ";
+// let upperFirstASC = 65;
+// //let lowerFirstASC = 97;
 
-let insertedTextDecode;
-let numberOffsetDecode;
-let decipherText = " ";
-let upperLastASC = 90;
-//let lowerLastASC = 122;
+// let insertedTextDecode;
+// let numberOffsetDecode;
+// let decipherText = " ";
+// let upperLastASC = 90;
+// //let lowerLastASC = 122;
 
-let alphabetLength = 26;
+// let alphabetLength = 26;
 
-function encode() {
-  insertedTextEncode = document.getElementById("inserted-text-encode").value.toUpperCase();
-  numberOffsetEncode = Number(document.getElementById("number-offset-encode").value);
-//colocar if e else para tratar letras minúsculas e caracteres especiais
-  for (let i = 0; i <insertedTextEncode.length; i++){
-    let letterCodeChipher = (((insertedTextEncode[i].charCodeAt(0) - upperFirstASC+ Number(numberOffsetEncode)) % alphabetLength) + upperFirstASC);
-    let resultCipher = String.fromCharCode(letterCodeChipher);
-    cipherText += resultCipher;
-  }
-  document.getElementById("final-text-encode").innerHTML = cipherText;
-}
+// function encode() {
+//   insertedTextEncode = document.getElementById("inserted-text-encode").value.toUpperCase();
+//   numberOffsetEncode = Number(document.getElementById("number-offset-encode").value);
+// //colocar if e else para tratar letras minúsculas e caracteres especiais
+//   for (let i = 0; i <insertedTextEncode.length; i++){
+//     let letterCodeChipher = (((insertedTextEncode[i].charCodeAt(0) - upperFirstASC+ Number(numberOffsetEncode)) % alphabetLength) + upperFirstASC);
+//     let resultCipher = String.fromCharCode(letterCodeChipher);
+//     cipherText += resultCipher;
+//   }
+//   document.getElementById("final-text-encode").innerHTML = cipherText;
+// }
+
+import cipher from "./cipher.js";
 
 let encodeButton = document.getElementById("encode-button");
-encodeButton.addEventListener("click", encode);
+encodeButton.addEventListener("click", cipher.encode);
 
-function decode() {
-  insertedTextDecode = document.getElementById("inserted-text-decode").value.toUpperCase();
-  numberOffsetDecode = Number(document.getElementById("number-offset-decode").value);
-  decipherText = " ";
+// function decode() {
+//   insertedTextDecode = document.getElementById("inserted-text-decode").value.toUpperCase();
+//   numberOffsetDecode = Number(document.getElementById("number-offset-decode").value);
+//   decipherText = " ";
 
-  for (let i = 0; i <insertedTextDecode.length; i++){
-    const letterCodeDechipher = (((insertedTextDecode[i].charCodeAt(0) - upperLastASC - Number(numberOffsetDecode)) % alphabetLength) + upperLastASC);
-    const resultDecipher = String.fromCharCode(letterCodeDechipher);
-    decipherText += resultDecipher;
-  }
-  document.getElementById("final-text-decode").textContent = decipherText;
-}
+//   for (let i = 0; i <insertedTextDecode.length; i++){
+//     const letterCodeDechipher = (((insertedTextDecode[i].charCodeAt(0) - upperLastASC - Number(numberOffsetDecode)) % alphabetLength) + upperLastASC);
+//     const resultDecipher = String.fromCharCode(letterCodeDechipher);
+//     decipherText += resultDecipher;
+//   }
+//   document.getElementById("final-text-decode").textContent = decipherText;
+// }
 
 let decodeButton = document.getElementById("decode-button");
-decodeButton.addEventListener("click", decode);
+decodeButton.addEventListener("click", cipher.decode);
